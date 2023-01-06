a45e150a937ef1626632598e1ad298651189a09f
milkcoke <mbh023@gmail.com>
2023-01-06 16:08:58 +0900

    ci: Update github action workflow
    Add permission about pages and id-token on deploy.yml.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,7 +37,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://milkcoke.github.io/docusaurus/docs/',
           showLastUpdateTime: true,
         },
         blog: {
@@ -46,7 +46,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://milkcoke.github.io/docusaurus/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.scss'),
